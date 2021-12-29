4b21e1e509404ff257b2ef82141fd772386e8904
Amir <40991530+amir04lm26@users.noreply.github.com>
2021-12-29 12:28:54 -0500

    feat(screen): set the user-agent as a mobile device

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,8 +1,9 @@
 import path from "path";
-console.log('node_env: ', process.env.TikTok_ENV)
 
 export const config = {
   TIKTOK_UTL: "https://tiktok.com/",
+  USER_AGENT:
+    "Mozilla/5.0 (Linux; Android 11; Surface Duo 2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Mobile Safari/537.36 EdgA/95.0.1020.42",
   ICON_PATH: path.join(__dirname, "src/assets/images/", "icon.png"),
   IS_DEV: process.env.TikTok_ENV !== "production",
 };
